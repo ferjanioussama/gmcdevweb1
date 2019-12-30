bc3a239b9f3068ec94fac3fae1b7bb0a5949014d
ferjanioussama <58654991+ferjanioussama@users.noreply.github.com>
2019-12-30 22:11:47 +0100

    Add files via upload

---
+++ check point 1/js/main.js
@@ -0,0 +1,38 @@
+function makeBold() {
+    if (document.getElementById("text").style.fontWeight == "normal")
+        document.getElementById("text").style.fontWeight = "bold";
+    else
+        document.getElementById("text").style.fontWeight = "normal";
+}
+
+
+function makeitalics() {
+    if (document.getElementById("text").style.fontStyle == "normal")
+        document.getElementById("text").style.fontStyle = "italic";
+    else
+        document.getElementById("text").style.fontStyle = "normal";
+
+}
+
+function makeunderline() {
+    if (document.getElementById("text").style.textDecoration == "none")
+        document.getElementById("text").style.textDecoration = "underline";
+    else
+        document.getElementById("text").style.textDecoration = "none";
+
+}
+
+function makebigger() {
+    var usersize = document.getElementById("selectfontsize").value;
+
+    document.getElementById("text").style.fontSize = usersize;
+
+}
+
+function changefont() {
+    var userfontfamily = document.getElementById("selectfontfamily").value;
+    document.getElementById("text").style.fontFamily = userfontfamily;
+
+
+
+}
